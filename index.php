ab98b685e4d9e7f510e0a26c96bba8d78de9d90c
Stevv-crypto <justforloginall@gmail.com>
2025-04-20 09:12:45 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita placeat nisi dignissimos quis aperiam inventore quo quibusdam nesciunt cupiditate sapiente, voluptatibus, hic illum! Aspernatur ea, expedita obcaecati odio aut deserunt!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
